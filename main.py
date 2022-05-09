f3c2829dc59b88e76a5e6ec2ef9302f3968b3c58
Bagrat <bagrat.arutyunov.888@gmail.com>
2022-05-09 20:47:28 +0300

    Improve UX

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
+import art
+
+
 def coder(word: str, shift: int, status: int):
     result_word = ""
     for index, letter in enumerate(word):
-        current_position = alphabet.index(letter)
-        new_position = (current_position + shift*status) % len(alphabet)
-        result_word += alphabet[new_position]
+        if letter in alphabet:
+            current_position = alphabet.index(letter)
+            new_position = (current_position + shift*status) % len(alphabet)
+            result_word += alphabet[new_position]
+        else:
+            result_word += letter
     return result_word
 
-
-coder_status = int(input("Type 1 to encrypt, type -1 to decrypt:\n"))
-word = input("Type your message:\n")
-shift = int(input("Type the shift:\n"))
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
             'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v',
             'w', 'x', 'y', 'z']
+app_working = 1
+print(art.logo)
+while app_working:
+    coder_status = int(input("Type 1 to encrypt, type -1 to decrypt:\n"))
+    word = input("Type your message:\n")
+    shift = int(input("Type the shift:\n"))
+    print(coder(word, shift, coder_status))
+    app_working = bool(input("Type 1 if you want to go again, otherwise type 0"))
 
-print(coder(word, shift, coder_status))
+
+
 
